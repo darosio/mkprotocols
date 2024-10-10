b54a6a8782bdb1cdc8180e4056389d833b257529
daniele arosio <daniele.arosio@cnr.it>
2024-10-10 16:30:51 +0200

    build: remove src and test

--- src/mkprotocols/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present daniele arosio <daniele.arosio@cnr.it>
-#
-# SPDX-License-Identifier: BSD-3-Clause
-__version__ = "0.0.1"
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present daniele arosio <daniele.arosio@cnr.it>
-#
-# SPDX-License-Identifier: BSD-3-Clause
